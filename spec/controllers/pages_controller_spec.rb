86485e3230be3f1ec22336f798938a701617cd17
Turki Marri <tk@motowiz.com>
2011-04-25 12:30:54 +0300

    Done exe + added title helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+    @base_title = "Sample App"
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                      :content => "Sample App | Home")
+                      :content => "")
     end
   end
 
@@ -25,11 +29,11 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                      :content => "Sample App | Contact")
+                      :content => @base_title)
     end
   end
   
-  describe "GET 'about" do
+  describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
@@ -37,8 +41,22 @@ describe PagesController do
   
   it "should have the right title" do
     get 'about'
+    
+      response.should have_selector("title",
+                    :content => @base_title)
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+  
+  it "should have right title" do
+    get 'help'
     response.should have_selector("title",
-                    :content => "Sample App | About")
+                    :content => @base_title)
     end
   end
 end
